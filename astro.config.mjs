21d8a2d9653d8c94de21afa151180cf1ca1ac7bb
Alloys Mila <mofax@users.noreply.github.com>
2023-12-10 10:25:45 +0300

    add html lang on post pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,36 @@
 import mdx from '@astrojs/mdx';
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
+
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 4080,
-  },
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'dracula',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
-  },
-  integrations: [mdx({
-    gfm: true,
-    drafts: true
-  })],
-  output: "server",
-  adapter: cloudflare()
+	site: 'https://alloysmi.la',
+	server: {
+		port: 4080
+	},
+	markdown: {
+		drafts: true,
+		shikiConfig: {
+			// Choose from Shiki's built-in themes (or add your own)
+			// https://github.com/shikijs/shiki/blob/main/docs/themes.md
+			theme: 'dracula',
+			// Add custom languages
+			// Note: Shiki has countless langs built-in, including .astro!
+			// https://github.com/shikijs/shiki/blob/main/docs/languages.md
+			langs: [],
+			// Enable word wrap to prevent horizontal scrolling
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			gfm: true,
+			drafts: true
+		}),
+		robotsTxt()
+	],
+	output: 'server',
+	adapter: cloudflare()
 });
